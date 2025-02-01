ea95b7b7632baf572e61d8cebc356397ddafebb9
sh4d0wy <sakshambhugra8@gmail.com>
2025-02-01 11:09:57 +0530

    fix: Fixed the increment score in contract

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -6,7 +6,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI = 1_000_000_000n;
 
-module.exports = buildModule("MetaPongModule", (m) => {
+module.exports = buildModule("PongGameModule", (m) => {
   // const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   // const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
